9924b13559eebb91677cc0450117ebc85e7be03c
Paul Nunez <paulnunezm@gmail.com>
2018-01-04 00:28:35 -0400

    edit: performed some code cleaning.

--- imageAutoAdder/imageAutoAdder.js
+++ imageAutoAdder/imageAutoAdder.js
@@ -2,20 +2,43 @@
 var addNewImageFrecuency = 1500
 
 var imgCount = 0,
-    intervalCounter = 0
-var addImageInterval = setInterval(function () {
+    intervalCounter = 0,
+    addImageInterval = setInterval(function () {
+        openImageAdderDialog()
+        setTimeout(addImage, 1000)
+    }, addNewImageFrecuency)
+
+function openImageAdderDialog() {
     $('#add-button').trigger('mouseenter')
     $('[data-action="picture"]').click()
-    setTimeout(function () {
-        if (imgCount === 0) {
-            imgCount = $(".modal-body .asset-list .asset-name").length - 1
-        }
-        $(".modal-body .asset-list .asset-name")[intervalCounter].click()
-        $(".modal-body .asset-list .asset-name")[intervalCounter].click()
-        if (intervalCounter >= imgCount) {
-            clearInterval(addImageInterval)
-        } else {
-            intervalCounter++
-        }
-    }, 1000)
-}, addNewImageFrecuency)
+}
+
+function addImage() {
+    getFilteredImageCount()
+    addImageToTheCanvas()
+    stopIntervalIfAllImagesAreAddedIncrementCounterIfNot()
+    incrementCounter()
+}
+
+function getFilteredImageCount() {
+    // initialize only once
+    if (imgCount === 0) {
+        imgCount = $(".modal-body .asset-list .asset-name").length - 1
+    }
+}
+
+function addImageToTheCanvas() {
+    // If we save the jQuery object in a variable it stops working
+    $(".modal-body .asset-list .asset-name")[intervalCounter].click()
+    $(".modal-body .asset-list .asset-name")[intervalCounter].click()
+}
+
+function stopIntervalIfAllImagesAreAddedIncrementCounterIfNot() {
+    if (intervalCounter >= imgCount) {
+        clearInterval(addImageInterval)
+    }
+}
+
+function incrementCounter() {
+    intervalCounter++
+}
