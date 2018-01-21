e7676483b8a053a65e96a04991d3267f4b331ec8
Abrahan Silverio <abrahansmg@gmail.com>
2018-01-21 14:36:42 -0400

    added layers preview and editor styles

--- uiEnhancer/enhancer.js
+++ uiEnhancer/enhancer.js
@@ -6,10 +6,11 @@
  **/
 (function (window, $) {
   if (window.enhacedUI !== undefined) {
-    console.warn('Already exits, or check if you are in the wrong console execution context.')
+    console.warn('Already installed, or check if you are in the right console execution context.')
     return
   }
 
+  var hoverTimeout
   window.enhacedUI = true
   var $document = $(document)
   var layersContainer = $('#layers-container')
@@ -21,9 +22,12 @@
     $('<style>' +
       '.hide-all, .lock-all  {color: #eee; padding:5px; width: 20px; height: 20px; position: absolute; top: 2px }' +
       '.hide-all { right: 40px }' + '.lock-all {right: 20px}' +
-      '.img-tooltip { display:none; position:absolute; border:1px solid #333; max-width:350px; background-color:#333; border-radius:5px; padding:10px; z-index:99999 }' +
+      '.img-tooltip { display:none; position:absolute; border:1px solid #333; max-width:350px; background-color:#333; border-radius:5px; box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5); padding:10px; z-index:99999 }' +
       '.img-tooltip img { width:100%; max-width:350px; max-heigh:350px; }' +
       '.hide-all.active, .lock-all.active  {color: #f60;}' +
+      '[data-type="hitarea"].ui-selected { background-color: rgba(74, 255, 255, 0.34) !important; }' +
+      '[data-type="image"].ui-selected { background-color: rgba(0, 194, 255, 0.16) !important; }' +
+      '#csseditor-panel .CodeMirror-sizer { background-color: #000 }' +
       '/*ends*/' +
     '</style>'))
 
@@ -33,18 +37,39 @@
   // Increase elments id field on inspector panel
   $document.find('#element-id-ref').width('92%')
 
-  // Add event delegation for buttons
+  // Add event for buttons
   hideAllBtn.on('click', function () {
-    layersContainer.find('[data-icon="q"]').not('span').trigger('click')
-    layersContainer.find('[data-icon="q"].controller').not('.disabled').length
-      ? $(this).addClass('active')
-      : $(this).removeClass('active')
+    setActiveBtn('q', $(this))
   })
 
   lockAllBtn.on('click', function () {
-    layersContainer.find('[data-icon="1"]').trigger('click')
-    layersContainer.find('[data-icon="1"].controller').not('.disabled').length
-      ? $(this).addClass('active')
-      : $(this).removeClass('active')
+    setActiveBtn('1', $(this))
+  })
+
+  function setActiveBtn (iconType, iconElement) {
+    layersContainer.find('[data-icon="' + iconType + '"]').trigger('click')
+    iconElement.toggleClass('active')
+  }
+
+  // Add event delegation for hovering layers
+  layersContainer.on('mouseenter', '.item:contains("image")', function (e) {
+    clearTimeout(hoverTimeout)
+    var currentLayer = this
+    hoverTimeout = setTimeout(function () {
+      $('<div class="img-tooltip"></div>')
+        .html($('<img />', { src: $('#device-screen #' + currentLayer.getAttribute('data-id') + ' img').attr('src') }))
+        .css({ top: e.pageY + 10, left: e.pageX + 20 })
+        .appendTo('body')
+        .fadeIn(300)
+    }, 1000)
+  })
+  .on('mouseleave', '.item:contains("image")', function (e) {
+    clearTimeout(hoverTimeout)
+    $('.img-tooltip').fadeOut(200, function () {
+      $('.img-tooltip').remove()
+    })
+  })
+  .on('mousemove', '.item:contains("image")', function (e) {
+    $('.img-tooltip').css({ top: e.pageY, left: e.pageX })
   })
 })(window, jQuery)
